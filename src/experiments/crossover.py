c6d46b24ff9e0e3002da3156a767fd98f96848bd
cassioss <cassiosantossousa@gmail.com>
2016-11-12 04:42:07 -0200

    Changed main class to geneflow and formulated crossover operation

--- src/experiments/crossover.py
+++ src/experiments/crossover.py
@@ -1,5 +1,33 @@
-fmax = 0.6
-fmin = 0.5
-favg = 0.5
+import numpy.random as random
 
-print (fmax - favg) / (fmax - fmin)
+def nonzero_rand(limit):
+	return random.randint(limit + 1)
+
+# Gets two distinct random positions in a list of  
+def crossover_two_point(ind1, ind2):
+	if len(ind1) < 2 or len(ind1) != len(ind2):
+		return
+
+	length = len(ind1)
+	if length == 2:
+		return ind1[:1] + ind2[1:], ind2[:1] + ind1[1:]
+
+	rand1 = nonzero_rand(length)
+	rand2 = nonzero_rand(length)
+
+	while (rand1 == rand2) or (rand1 == 0 and rand2 == length) or (rand2 == 0 and rand1 == length):
+		rand2 = nonzero_rand(length)
+
+	rand1, rand2 = (rand1, rand2) if rand1 < rand2 else (rand2, rand1)
+
+	one_crossed = ind1[:rand1] + ind2[rand1:rand2] + ind1[rand2:]
+	two_crossed = ind2[:rand1] + ind1[rand1:rand2] + ind2[rand2:]
+
+	if one_crossed == ind1 or two_crossed == ind1:
+		print rand1, rand2
+		raise ValueError('Error')
+
+	return one_crossed, two_crossed
+
+for i in range(2000):
+	print crossover_two_point([0,2,4,6,8,10], [1,3,5,7,9,11])
